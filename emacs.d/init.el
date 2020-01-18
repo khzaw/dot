573ebf4c63fbb75ca253966345dfd864f51f85ba
Kaung Htet Zaw <me@kaunghtet.xyz>
2020-01-18 21:53:09 +0800

    Update evil settings and install protobuf mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -25,6 +25,8 @@
                                   web-mode
                                   centered-window
                                   prettier-js
+                                  protobuf-mode
+                                  markdown-mode
                                     ))
 (package-initialize)
 (setq package-enable-at-startup nil)
@@ -49,7 +51,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(fzf evil magit smooth-scrolling whick-key org-bullets one-themes company-mode fzf\.el)))
+   '(markdown-mode protobuf-mode fzf evil magit smooth-scrolling whick-key org-bullets one-themes company-mode fzf\.el)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
