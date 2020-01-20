6f78cdc184b897012b42ec6656a58a12d8cfa7ab
Kaung Htet Zaw <me@kaunghtet.xyz>
2020-01-21 02:12:56 +0800

    Use use-package for all package dependencies

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,55 +1,32 @@
-(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
-(add-to-list 'default-frame-alist '(ns-appearance . dark))
+(add-to-list 'default-frame-alist '
+  (ns-transparent-titlebar . t))
+(add-to-list 'default-frame-alist '
+  (ns-appearance . dark))
 
 (setq custom-file "~/.emacs.d/custom.el")
-(load custom-file)
+(ignore-errors 
+  (load custom-file))
 
 (require 'package)
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
+(setq package-archives '
+  (
+    ("org"       . "https://orgmode.org/elpa/")
+    ("gnu" . "https://elpa.gnu.org/packages/")
+    ("melpa"     . "https://melpa.org/packages/")
+    ("melpa-stable" . "https://stable.melpa.org/packages/")))
 
-(setq package-selected-packages '(evil
-                                  evil-escape
-                                  editorconfig
-                                  magit
-                                  smooth-scrolling
-                                  dired-x
-                                  whick-key
-                                  org-bullets
-                                  company-mode
-                                  fzf
-                                  one-themes
-                                  doom-themes
-                                  moe-theme
-                                  cyberpunk-theme
-                                  minimal-theme
-                                  grandshell-theme
-                                  poet-theme
-                                  web-mode
-                                  centered-window
-                                  prettier-js
-                                  protobuf-mode
-                                  markdown-mode
-                                  helm
-                                  helm-projectile
-                                  restart-emacs
-                                  projectile
-                                  fancy-battery
-                                    ))
-(package-initialize)
-(setq package-enable-at-startup nil)
 (unless package-archive-contents
   (package-refresh-contents))
-(package-install-selected-packages)
 
-;; Load theme
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(package-initialize)
 
-(load-theme 'doom-molokai t)
+(unless 
+  (package-installed-p 'use-package)
+  (package-install 'use-package))
 
-;; Load mail config file ~/.config.org
+(require 'use-package)
+(setq use-package-always-ensure t)
+
 (require 'org)
-(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
-
-(provide 'init)
+(org-babel-load-file 
+  (expand-file-name "~/.emacs.d/config.org"))
