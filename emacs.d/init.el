c17c02cf775d133e0455524a23a878968aeb5ce7
Kaung Htet Zaw <me@kaunghtet.xyz>
2020-02-27 17:52:04 +0800

    FOrmat

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -2,20 +2,16 @@
 ;;; Commentary:
 ;;; Emacs Startup File --- initialization for Emacs
 ;;; Code:
-(add-to-list 'default-frame-alist '
-  (ns-transparent-titlebar . t))
-(add-to-list 'default-frame-alist '
-  (ns-appearance . dark))
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+(add-to-list 'default-frame-alist '(ns-appearance . dark))
 
 (setq custom-file "~/.emacs.d/custom.el")
 (ignore-errors (load custom-file))
 
 (require 'package)
-(setq package-archives '
-  (
-    ("melpa"     . "https://melpa.org/packages/")
-    ("org"       . "https://orgmode.org/elpa/")
-    ("gnu" . "https://elpa.gnu.org/packages/")))
+(setq package-archives '(("melpa"     . "https://melpa.org/packages/")
+                         ("org"       . "https://orgmode.org/elpa/")
+                         ("gnu" . "https://elpa.gnu.org/packages/")))
 
 (unless package-archive-contents
   (package-refresh-contents))
@@ -24,8 +20,8 @@
 
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
-
 (require 'use-package)
+(require 'use-package-ensure)
 (setq use-package-always-ensure t)
 
 (require 'org)
