ef113b0c338fbeba137313e6b1cc036401872be0
Kaung Htet Zaw <kayhzaw@gmail.com>
2021-12-18 02:22:53 +0800

    Update xmonad

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,42 +1,58 @@
-import XMonad
-import XMonad.Config.Desktop
+-- import XMonad
+-- import XMonad.Config.Desktop
+-- 
+-- import XMonad.Util.EZConfig
+-- import XMonad.Util.Ungrab
+-- 
+-- import XMonad.Hooks.EwmhDesktops
+-- import XMonad.Hooks.DynamicLog
+-- 
+-- myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+-- 
+-- myConfig = def
+--   { terminal   = "alacritty"
+--   , modMask    = mod4Mask
+--   , borderWidth = 2
+--   , workspaces = myWorkspaces
+--   }
+--   `additionalKeysP`
+--   [ ("M-S-4"    , unGrab *> spawn "scrot -s")
+--   , ("M-<Space>", spawn "rofi -show run -dpi 164")
+--   , ("M-]"      , spawn "google-chrome-beta" )
+--   , ("M-S-]"    , spawn "firefox")
+--   , ("M-M1-\\"   , spawn "1password")
+--   ]
+-- 
+-- main :: IO ()
+-- main = xmonad . ewmh =<< myConfig
 
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run
 import XMonad.Util.EZConfig
 import XMonad.Util.Ungrab
+import System.IO
 
-import XMonad.Layout.ThreeColumns
-import XMonad.Layout.Magnifier
-import XMonad.Layout.Gaps
-import XMonad.Layout.Spacing
+myManageHook = composeAll
+	[ className =? "1password"	--> doFloat
+	]
 
-import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.DynamicLog
-
-myLayout = tiled ||| Mirror tiled ||| Full ||| threeCol
-  where
-    threeCol = magnifiercz' 1.3 $ ThreeColMid nmaster delta ratio 
-    tiled    = Tall nmaster delta ratio
-    nmaster  = 1     -- Default number of windows in the master pane
-    ratio    = 1/2   -- Default proportion of screen occupied by master pane
-    delta    = 3/100 -- Percent of screen to increment by when resizing panes
-
-myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-
-myConfig = def
-  { terminal   = "alacritty"
-  , modMask    = mod4Mask
-  , borderWidth = 2
-  , layoutHook = spacingRaw False (Border 10 10 10 10) True (Border 10 10 10 10) True $ layoutHook def
-  , workspaces = myWorkspaces
-  }
-  `additionalKeysP`
-  [ ("M-S-4"    , unGrab *> spawn "scrot -s")
-  , ("M-<Space>", spawn "rofi -show run -dpi 164")
-  , ("M-]"      , spawn "brave-beta" )
-  , ("M-S-]"    , spawn "firefox")
-  , ("M-M1-\\"   , spawn "1password")
-  ]
-
-main :: IO ()
-main = xmonad . ewmh =<< xmobar myConfig
+main = do 
+	xmproc <- spawnPipe "xmobar"
+	xmonad $ docks defaultConfig 
+		{ manageHook = myManageHook <+> manageHook defaultConfig
+		, layoutHook = avoidStruts $ layoutHook defaultConfig
+		, logHook = dynamicLogWithPP xmobarPP
+			{ ppOutput = hPutStrLn xmproc
+			, ppTitle = xmobarColor "green" "" . shorten 50
+			}
+		, modMask = mod4Mask
+		, borderWidth = 1
+		, terminal = "alacritty"
+		} `additionalKeysP`
+		[ ("M-<Space>" , spawn "rofi -show run -dpi 164") 
+		-- , ("M-S-4"     , unGrab *> spawn "scrot -s")
+		]
+	
 
