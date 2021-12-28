3fda38fe927cb43b8d6fc5edb0c5073b24c354f0
Kaung Htet Zaw <kayhzaw@gmail.com>
2021-12-29 04:19:40 +0800

    Add more custom options, refactor, add more shortcuts and hooks

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -30,43 +30,104 @@ import System.IO
 
 import XMonad
 import XMonad.Layout.Spacing
+import XMonad.Layout.Gaps
+import XMonad.Layout.LayoutModifier (ModifiedLayout)
 
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.SetWMName
 
 import XMonad.Util.Run
 import XMonad.Util.EZConfig
 import XMonad.Util.Ungrab
 
 myManageHook = composeAll
-	[ className =? "1Password"	--> doCenterFloat
-        , className =? "Nitrogen"       --> doCenterFloat
-        , className =? "feh"            --> doFloat
-        , title     =? "Save As"        --> doCenterFloat
-        , title     =? "Save Image"     --> doCenterFloat
+	[ className =? "1Password"			--> doCenterFloat
+        , className =? "Nitrogen"			--> doCenterFloat
+        , className =? "feh"				--> doCenterFloat
+        , className =? "Plugins"			--> doCenterFloat
+        , className =? "file_progress"			--> doFloat
+        , className =? "pinentry-gtk-2"			--> doFloat
+        , className =? "toolbar"			--> doFloat
+        , className =? "notification"			--> doFloat
+        , className =? "error"				--> doFloat
+	, className =? "stalonetray"			--> doIgnore
+	, className =? "trayer"				--> doIgnore
+        , className =? "calculator"	  		--> doFloat
+        , className =? "hl_linux"	  		--> doFloat
+        , title     =? "Volume Control"			--> doCenterFloat
+        , title     =? "Bluetooth Devices"		--> doCenterFloat
+        , title     =? "Save As"			--> doCenterFloat
+        , title     =? "Save Image"			--> doCenterFloat
         , title     =? "Enter name of file to save to"  --> doCenterFloat
+        , title     =? "Picture in picture"  		--> doFloat
+        , title     =? "MetaMask Notification" 		--> doFloat
+        , title     =? "Media viewer"	 		--> doFloat
         ]
 
-myGap = 10
+-- StartupHook
+myStartupHook :: X ()
+myStartupHook = setWMName "LG3D"
 
+-- Variables
+myTerminal :: String
+myTerminal = "alacritty"
+
+myBrowser :: String
+myBrowser = "google-chrome-beta"
+
+myLauncher :: String
+myLauncher = "rofi -show run -dpi 196 -show-icons"
+
+myDiscord :: String
+myDiscord = "discord"
+
+myTelegram :: String
+myTelegram = "telegram-desktop"
+
+my1Password :: String
+my1Password = "1password"
+
+mySpotify :: String
+mySpotify = "spotify"
+
+-- Key Bindings
 -- M1 - Left Alt
 -- M  - Mod Key (Cmd)
+myKeys :: [(String, X ())]
+myKeys =
+	[ ("M1-<Space>" , spawn myLauncher)
+	, ("M1-S-3"	, unGrab *> spawn "scrot ~/Pictures/screenshots/")
+	, ("M1-S-4"	, unGrab *> spawn "scrot -s ~/Pictures/screenshots/")
+	, ("M1-\\"	, spawn my1Password)
+	, ("M-x s"	, spawn mySpotify)
+	, ("M-x b"	, spawn myBrowser)
+	, ("M-x t"	, spawn myTelegram)
+	, ("M-x d"	, spawn myDiscord)
+	]
+
+-- Layouts
+mySpacing :: Integer -> l a -> XMonad.Layout.LayoutModifier.ModifiedLayout Spacing l a
+mySpacing i = spacingRaw False (Border i i i i) True (Border i i i i) True
+
+
 main = do 
-	xmproc <- spawnPipe "xmobar"
+	xmproc <- spawnPipe "xmobar -d"
+        tray <- spawnPipe "killall -q stalonetray; sleep 1; stalonetray"
 	xmonad $ docks defaultConfig 
 		{ manageHook = myManageHook <+> manageHook defaultConfig
-		, layoutHook = avoidStruts $ spacingRaw True (Border myGap myGap myGap myGap) True (Border myGap myGap myGap myGap) True $ layoutHook defaultConfig
+		, layoutHook = avoidStruts $ mySpacing 10 $ layoutHook defaultConfig
 		, logHook = dynamicLogWithPP xmobarPP
 			{ ppOutput = hPutStrLn xmproc
-			, ppTitle = xmobarColor "green" "" . shorten 100
+			, ppTitle = xmobarColor "#8bc34a" "" . shorten 50
 			}
 		, modMask = mod4Mask
+		, handleEventHook = fullscreenEventHook
 		, borderWidth = 1
-		, terminal = "alacritty"
-		} `additionalKeysP`
-		[ ("M1-<Space>" , spawn "rofi -show run -dpi 1 -show-icons") 
-		, ("M1-S-4"     , unGrab *> spawn "scrot -s")
-		]
+		, terminal = myTerminal
+                , startupHook = myStartupHook
+		} `additionalKeysP` myKeys
 	
 
