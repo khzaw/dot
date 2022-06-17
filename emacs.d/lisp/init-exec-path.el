85ad2301571223ed58ff8414e0baa463286fec24
Kaung Htet Zaw <kayhzaw@gmail.com>
2022-06-18 01:05:12 +0800

    Native compilation

--- emacs.d/lisp/init-exec-path.el
+++ emacs.d/lisp/init-exec-path.el
@@ -2,7 +2,19 @@
   :if (memq window-system '(mac ns x))
   :config
   (setq exec-path-from-shell-variables '("PATH" "GOPATH"))
-  (exec-path-from-shell-initialize))
+  (exec-path-from-shell-initialize)
+  (if (and (fboundp 'native-comp-available-p)
+        (native-comp-available-p))
+    (progn
+      (message "Native comp is available")
+      (add-to-list 'exec-path (concat invocation-directory "bin") t)
+      (setenv "LIBRARY_PATH" (concat (getenv "LIBRARY_PATH")
+                               (when (getenv "LIBRARY_PATH")
+                                 ":")
+                               (car (file-expand-wildcards
+                                      (expand-file-name "~/homebrew/opt/gcc/lib/gcc/*")))))
+      (setq comp-deferred-compilation t))
+    (message "Native comp is *not* available")))
 
 (provide 'init-exec-path)
 ;;; init-exec-path.el ends here
