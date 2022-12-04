f8fcfd0efad2c3e76de34806bd64bb6669f6f981
Kaung Htet <kayhzaw@gmail.com>
2022-12-05 01:59:05 +0800

    Tweak eglot to add code-actions bindings

--- emacs.d/lisp/init-eglot.el
+++ emacs.d/lisp/init-eglot.el
@@ -1,19 +1,36 @@
 (use-package eglot
-  :ensure nil
   :commands (eglot eglot-ensure)
-  :hook ((go-mode . eglot-ensure)
-          (rust-mode . eglot-ensure)
-          (typescript-mode . eglot-ensure)
-          (python-mode . eglot-ensure))
+  :hook ((go-mode
+           web-mode
+           js-mode
+           python-mode
+           rust-mode
+           typescript-mode) . eglot-ensure)
+  :custom (eglot-autoshutdown t)
+  :bind (("C-c e f" . #'eglot-format)
+          ("C-c e a" . #'eglot-code-actions)
+          ("C-c e i " . #'eglot-code-action-organize-imports))
   :config
   (setq eglot-strict-mode nil)
   (setq eglot-confirm-server-initiated-edits nil)
-  (setq completion-category-overrides '((eglot (styles orderless))))
-  :custom
-  (eglot-autoshutdown t))
+  ;; (setq completion-category-overrides '((eglot (styles orderless))))
+
+  (defun eglot-capf ()
+    (setq-local completion-at-point-functions
+      (list (cape-super-capf
+              #'eglot-completion-at-point
+              #'tempel-expand
+              #'cape-file))))
+
+  (defun eglot-actions-before-save ()
+    (add-hook 'before-save-hook (lambda ()
+                                  (call-interactively #'eglot-format)
+                                  (call-interactively #'eglot-code-action-organize-imports))))
+  (add-hook 'eglot-managed-mode-hook #'eglot-actions-before-save)
+  (add-hook 'eglot-managed-mode-hook #'eglot-capf))
 
 (use-package consult-eglot
   :defer t
-  :after (consult eglot))
+  :after (consult eglot vertico))
 
 (provide 'init-eglot)
