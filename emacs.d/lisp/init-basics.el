87778c49f5ce669eb152ba21b632ef8b4f7c8280
Kaung Htet <kayhzaw@gmail.com>
2022-12-05 01:58:28 +0800

    Install packages Helpful

--- emacs.d/lisp/init-basics.el
+++ emacs.d/lisp/init-basics.el
@@ -144,9 +144,8 @@
 ;; Minimum window height
 (setq window-min-height 1)
 
-;; Native compilation
-(setq native-comp-async-report-warnings-errors nil) ;; silence compiler warnings
-(add-to-list 'native-comp-eln-load-path (expand-file-name "eln-cache/" user-emacs-directory))
+;; Follow symlinks for vc
+(setq vc-follow-symlinks t)
 
 ;; Buffer encoding
 (prefer-coding-system       'utf-8)
@@ -234,6 +233,29 @@
   :config
   (setq prescient-sort-full-matches-first t
     prescient-sort-length-enable t))
+
+(use-package help-mode
+  :ensure nil
+  :bind
+  ("C-h K" . describe-keymap)
+  (:map help-mode-map
+    ("<" . help-go-back)
+    (">" . help-go-forward)))
+
+(use-package helpful
+  :bind
+  ([remap describe-command] . helpful-command)
+  ([remap describe-function] . helpful-callable)
+  ([remap describe-key] . helpful-key)
+  ([remap describe-symbol] . helpful-symbol)
+  ([remap describe-variable] . helpful-variable)
+  ("C-h F" . helpful-function)
+  :config
+  (with-eval-after-load 'evil
+    (evil-define-keys* 'motion helpful-mode-map
+      (kbd "gr") #'helpful-update
+      (kbd "<tab>") #'forward-button))
+  :custom (helpful-max-buffers 2))
 
 (provide 'init-basics)
 ;;; init-basics.el ends here
