269280d80323aa51bf1b8191aa4737149a2020ec
Kaung Htet <kayhzaw@gmail.com>
2023-06-11 23:28:15 +0800

    Load SSH_* env variables into emacs in linux

--- emacs.d/lisp/init-exec-path.el
+++ emacs.d/lisp/init-exec-path.el
@@ -4,17 +4,21 @@
   (setq exec-path-from-shell-variables '("PATH" "GOPATH"))
   (exec-path-from-shell-initialize)
   (if (and (fboundp 'native-comp-available-p)
-        (native-comp-available-p))
+           (native-comp-available-p))
+      (progn
+        (message "Native comp is available")
+        (add-to-list 'exec-path (concat invocation-directory "bin") t)
+        (setenv "LIBRARY_PATH" (concat (getenv "LIBRARY_PATH")
+                                       (when (getenv "LIBRARY_PATH") ":")
+                                       (car (file-expand-wildcards
+                                             (expand-file-name "~/homebrew/opt/gcc/lib/gcc/*")))))
+        (setq native-comp-deferred-compilation t))
+    (message "Native comp is *not* available"))
+
+  (when (eq system-type 'gnu/linux)
     (progn
-      (message "Native comp is available")
-      (add-to-list 'exec-path (concat invocation-directory "bin") t)
-      (setenv "LIBRARY_PATH" (concat (getenv "LIBRARY_PATH")
-                               (when (getenv "LIBRARY_PATH")
-                                 ":")
-                               (car (file-expand-wildcards
-                                      (expand-file-name "~/homebrew/opt/gcc/lib/gcc/*")))))
-      (setq native-comp-deferred-compilation t))
-    (message "Native comp is *not* available")))
+      (exec-path-from-shell-copy-env "SSH_AGENT_PID")
+      (exec-path-from-shell-copy-env "SSH_AUTH_SOCK"))))
 
 (use-package auto-compile
   :config
