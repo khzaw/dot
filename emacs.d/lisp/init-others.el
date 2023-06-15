9ac14d5945432ec0fc9e19bbd19de4cc2eead733
Kaung Htet <kayhzaw@gmail.com>
2023-06-16 02:34:04 +0800

    Read epub books in emacs

--- emacs.d/lisp/init-others.el
+++ emacs.d/lisp/init-others.el
@@ -7,8 +7,20 @@
   :bind ("C-c q" . chatgpt-query))
 
 (use-package nov
-  :mode ("\\.epub\\'" . nov-mode))
+  :mode ("\\.epub\\'" . nov-mode)
+  :config
+  (defun khz/nov-font-setup()
+    (face-remap-add-relative 'variable-pitch :family "Vollkorn" :height 1.2)))
+
+(use-package nov-xwidget
+  :straight (:type git :host github :repo "chenyanming/nov-xwidget")
+  :after nov
+  :config
+  (define-key nov-mode-map (kbd "o") 'nov-xwidget-view)
+  (add-hook 'nov-mode-hook 'nov-xwidget-inject-all-files))
 
 (use-package emojify :config (global-emojify-mode))
+
+(use-package calibredb)
 
 (provide 'init-others)
