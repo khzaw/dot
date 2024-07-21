7c376f4f536dab0af39c4b7a4cf639f4bbd55c81
Kaung Htet <kayhzaw@gmail.com>
2024-07-21 12:02:48 +0800

    Tweak treesitter-context mode

--- emacs.d/lisp/init-treesitter.el
+++ emacs.d/lisp/init-treesitter.el
@@ -21,15 +21,20 @@
   (global-treesit-auto-mode))
 
 (use-package treesitter-context
-  :after (treesit-auto posframe-plus)
   :straight (:type git :host github :repo "zbelial/treesitter-context.el" :files ("*.el"))
-  :commands (treesitter-context-toggle-show)
   :config
   (setq treesitter-context-idle-time 0.5
         treesitter-context-show-context-always t
-        treesitter-context-frame-autohide-timeout 15))
+        treesitter-context-frame-autohide-timeout 15)
+  :hook ((go-ts-mode . treesitter-context-focus-mode)
+         (typescript-ts-mode . treesitter-context-focus-mode)
+         (python-ts-mode . treesitter-context-focus-mode)
+         (yaml-ts-mode . treesitter-context-focus-mode))
+  :bind (("C-c e C-t" . treesitter-context-toggle-show)
+         ("C-c e F" . treesitter-context-focus-mode)))
 
 (use-package combobulate
+  :disabled t
   :straight (combobulate :type git :host github :repo "mickeynp/combobulate" :branch "development")
   :custom
   (combobulate-key-prefix "C-c o")
@@ -73,18 +78,69 @@
   :straight (evil-textobj-tree-sitter :type git
                                       :host github
                                       :repo "meain/evil-textobj-tree-sitter"
-                                      :files (:defaults "queries"))
+                                      :files (:defaults "queries" "treesit-queries"))
   :config
-  (defvar +tree-sitter-inner-text-objects-map (make-sparse-keymap))
-  (defvar +tree-sitter-outer-text-objects-map (make-sparse-keymap))
-  (defvar +tree-sitter-goto-previous-map (make-sparse-keymap))
-  (defvar +tree-sitter-goto-next-map (make-sparse-keymap))
+  ;; (defvar +tree-sitter-inner-text-objects-map (make-sparse-keymap))
+  ;; (defvar +tree-sitter-outer-text-objects-map (make-sparse-keymap))
+  ;; (defvar +tree-sitter-goto-previous-map (make-sparse-keymap))
+  ;; (defvar +tree-sitter-goto-next-map (make-sparse-keymap))
 
-  (evil-define-key '(visual operator) 'tree-sitter-mode
-    "i" +tree-sitter-inner-text-objects-map
-    "a" +tree-sitter-outer-text-objects-map)
-  (evil-define-key 'normal 'tree-sitter-mode
-    "[g" +tree-sitter-goto-previous-map
-    "]g" +tree-sitter-goto-next-map))
+  ;; (evil-define-key '(visual operator) 'tree-sitter-mode
+  ;;   "i" +tree-sitter-inner-text-objects-map
+  ;;   "a" +tree-sitter-outer-text-objects-map)
+  ;; (evil-define-key 'normal 'tree-sitter-mode
+  ;;   "[g" +tree-sitter-goto-previous-map
+  ;;   "]g" +tree-sitter-goto-next-map))
+
+  ;; bind `function.outer`(entire function block) to `f` for use in things like `vaf`, `yaf`
+  (define-key evil-outer-text-objects-map "f" (evil-textobj-tree-sitter-get-textobj "function.outer"))
+  ;; bind `function.inner`(function block without name and args) to `f` for use in things like `vif`, `yif`
+  (define-key evil-inner-text-objects-map "f" (evil-textobj-tree-sitter-get-textobj "function.inner"))
+
+  ;; You can also bind multiple items and we will match the first one we can find
+  (define-key evil-outer-text-objects-map "a" (evil-textobj-tree-sitter-get-textobj ("conditional.outer" "loop.outer")))
+
+  ;; The first arguemnt to `evil-textobj-tree-sitter-get-textobj' will be the capture group to use
+  ;; and the second arg will be an alist mapping major-mode to the corresponding query to use.
+  (define-key evil-outer-text-objects-map "m" (evil-textobj-tree-sitter-get-textobj "import"
+                                                '((python-mode . [(import_statement) @import])
+                                                  (rust-mode . [(use_declaration) @import]))))
+  ;; Goto start of next function
+  (define-key evil-normal-state-map
+    (kbd "]f")
+    (lambda ()
+      (interactive)
+      (evil-textobj-tree-sitter-goto-textobj "function.outer")))
+
+  ;; Goto start of previous function
+  (define-key evil-normal-state-map
+    (kbd "[f")
+    (lambda ()
+      (interactive)
+      (evil-textobj-tree-sitter-goto-textobj "function.outer" t)))
+
+  ;; Goto end of next function
+  (define-key evil-normal-state-map
+    (kbd "]F")
+    (lambda ()
+      (interactive)
+      (evil-textobj-tree-sitter-goto-textobj "function.outer" nil t)))
+
+  ;; Goto end of previous function
+  (define-key evil-normal-state-map
+    (kbd "[F")
+    (lambda ()
+      (interactive)
+      (evil-textobj-tree-sitter-goto-textobj "function.outer" t t))))
+
+(use-package treesit-jump
+  :straight (:host github :repo "dmille56/treesit-jump" :files ("*.el" "treesit-queries"))
+  :config
+  ;; Optional: add some queries to filter out of results (since they can be too cluttered sometimes)
+  (setq treesit-jump-queries-filter-list '("inner" "test" "param")))
+
+(use-package treesit-fold
+  :straight (treesit-fold :type git :host github :repo "emacs-tree-sitter/treesit-fold"))
+
 
 (provide 'init-treesitter)
