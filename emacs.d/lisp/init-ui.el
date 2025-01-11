5fc1da83fafc5965a83c3b5e521bacdda0bddaec
Kaung Htet <kaung.htet@foodpanda.com>
2025-01-11 23:32:35 +0800

    Update font name

--- emacs.d/lisp/init-ui.el
+++ emacs.d/lisp/init-ui.el
@@ -15,15 +15,15 @@
 ;; UI
 (when (eq system-type 'gnu/linux)
   (setq-default line-spacing 0.05)
-  (set-face-attribute 'default nil :font "TX-02" :weight 'medium :height 90)
-  (set-face-attribute 'fixed-pitch nil :font "TX-02" :weight 'normal :height 1.0)
-  (set-face-attribute 'variable-pitch nil :font "TX-02" :weight 'normal))
+  (set-face-attribute 'default nil :font "Berkeley Mono" :weight 'medium :height 90)
+  (set-face-attribute 'fixed-pitch nil :font "Berkeley Mono" :weight 'normal :height 1.0)
+  (set-face-attribute 'variable-pitch nil :font "Berkeley Mono" :weight 'normal))
 
 (when (eq system-type 'darwin)
   (setq-default line-spacing 1)
-  (set-face-attribute 'default nil :font "TX-02" :weight 'medium :height 140 :width 'condensed)
-  (set-face-attribute 'fixed-pitch nil :font "TX-02" :weight 'normal :height 1.0)
-  (set-face-attribute 'variable-pitch nil :font "TX-02" :weight 'normal :height 1.0))
+  (set-face-attribute 'default nil :font "Berkeley Mono" :weight 'medium :height 140 :width 'condensed)
+  (set-face-attribute 'fixed-pitch nil :font "Berkeley Mono" :weight 'normal :height 1.0)
+  (set-face-attribute 'variable-pitch nil :font "Berkeley Mono" :weight 'normal :height 1.0))
 
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
