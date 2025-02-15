d2048853e4940be9bc8e952da01d18be5b00faa3
Kaung Htet <kayhzaw@gmail.com>
2025-02-15 17:08:11 +0800

    Add moody front space

--- emacs.d/lisp/init-modeline.el
+++ emacs.d/lisp/init-modeline.el
@@ -14,6 +14,7 @@
 (use-package moody
   :config
   (setq x-underline-at-descent-line t)
+  (moody-replace-mode-line-front-space)
   (moody-replace-mode-line-buffer-identification)
   (moody-replace-vc-mode)
   (moody-replace-eldoc-minibuffer-message-function))
