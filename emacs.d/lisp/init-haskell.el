a43ca4ebdd0a5b6b528ea816b218244446ae2089
Kaung Htet <kayhzaw@gmail.com>
2025-05-04 03:29:42 +0800

    Temporarily disable haskell-mode

--- emacs.d/lisp/init-haskell.el
+++ emacs.d/lisp/init-haskell.el
@@ -1,16 +1,6 @@
 ;; -*- lexical-binding: t; -*-
 
-(use-package haskell-mode
-  :straight (haskell-mode :type git
-                          :host github
-                          :repo "haskell/haskell-mode"))
-
-(use-package haskell-ts-mode
-  :disabled t
-  :straight (:type git :host codeberg :repo "pranshu/haskell-ts-mode")
-  :config
-  (setq haskell-ts-highlight-signature t))
-
+(use-package haskell-mode :disabled t)
 
 (provide 'init-haskell)
 ;;; init-haskell.el ends here
