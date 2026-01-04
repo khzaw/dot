9e1cf16dc356ed64c8fe44ab90c48c1e66e68ca1
Kaung Htet <kayhzaw@gmail.com>
2026-01-04 21:20:37 +0800

    Add more ai tools

--- emacs.d/lisp/init-ai.el
+++ emacs.d/lisp/init-ai.el
@@ -7,4 +7,10 @@
   (claude-code-ide-emacs-tools-setup)
   (setq claude-code-ide-window-side 'right))
 
+(use-package agent-shell
+  :straight (:type git :host github :repo "xenodium/agent-shell"))
+
+(use-package eca
+  :straight (:type git :host github :repo "editor-code-assistant/eca-emacs" :files ("*.el")))
+
 (provide 'init-ai)
