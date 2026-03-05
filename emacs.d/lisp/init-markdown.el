0cef3525ca54d7270bc028b4166c3ce19242f4e8
Kaung Htet <kayhzaw@gmail.com>
2026-03-05 22:52:02 +0800

    markdown-mode: upgrade version

--- emacs.d/lisp/init-markdown.el
+++ emacs.d/lisp/init-markdown.el
@@ -85,7 +85,7 @@
       (let ((tmp (buffer-name)))
         (set-buffer buffer)
         (set-buffer (markdown tmp))
-        (format "<!DOCTYPE html><html><title>Markdown Preview</title><link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.1.0/github-markdown.min.css\"/><body><article class=\"markdown-body\" style=\"box-sizing: border-box;min-width:200px; max-width:980px;margin: 0 auto; padding: 45px;\">%s</article></body></html>" (buffer-string))))))
+        (format "<!DOCTYPE html><html><title>Markdown Preview</title><link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.8.1/github-markdown.min.css\"/><body><article class=\"markdown-body\" style=\"box-sizing: border-box;min-width:200px; max-width:980px;margin: 0 auto; padding: 45px;\">%s</article></body></html>" (buffer-string))))))
 
 (defun markdown-realtime-preview ()
   "Preview markdown."
